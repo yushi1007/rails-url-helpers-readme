d1e9cdb3e821b0e4ca826cf6157a9c22d3fd1374
Yu Shi <yushiys95@gmail.com>
2021-02-23 21:49:14 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
 end
+
+
+# Prefix Verb  URI    Pattern      Controller#Action
+# posts  GET  /posts(.:format)     posts     #index
+# post   GET  /posts/:id(.:format) posts     #show
+
+# GET (This is HTTP verb)
